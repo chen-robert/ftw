974a312d0bff2a21fefd6e8f19f7f6bb0d8bd1c8
gamester543 <gamesterrex@gmail.com>
2018-04-04 22:10:10 -0700

    Hopefully fixed config files

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 /*eslint no-console: "warn"*/
-'use strict';
+"use strict";
+const PORT = 5000
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-app.use(express.static('public'))
+app.use(express.static("public"))
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
-app.listen(3000, () => console.log('Hello! Listening on port 3000!'));
+http.listen(PORT, () => console.log("Hello! Listening on port 3000!"));
+
+io.on("connection", function (socket) {
+    console.log("HI");
+});
