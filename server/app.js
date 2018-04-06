501551b821dccda0b9458cbbfd12691410463526
gamester543 <gamesterrex@gmail.com>
2018-04-05 23:17:14 -0700

    Attached login POST to MongoDB

--- server/app.js
+++ server/app.js
@@ -4,10 +4,15 @@ const PORT = process.env.PORT || 5000
 
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 const http = require("http").Server(app);
-const login = require("./auth/login.js");
+const auth = require("./auth/login.js");
 const io = require("socket.io")(http);
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 app.use(express.static("public"))
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
@@ -17,7 +22,10 @@ http.listen(PORT, () => console.log("Hello! Listening on port 3000!"));
 
 app.post("/login", function (req, res) {
     if (req.body.username && req.body.password) {
-        console.log("Success!");
+        auth.login(req.body.username, req.body.password, function (err, user) {
+            if (err) res.send("TODO: Error page");
+            return res.send("/public/resources/index.html");
+        });
     }
 })
 
