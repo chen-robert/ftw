9a288cb2dad20eb09dcaa3163ff15b78ea206e4b
gamester543 <gamesterrex@gmail.com>
2018-04-11 00:30:15 -0700

    Added link parsing!

--- server/game/chat/chatUtils.js
+++ server/game/chat/chatUtils.js
@@ -28,6 +28,7 @@ class ChatManager {
             if (typeof message !== "string" || message.length == 0) return;
 
             message = _self.clean(message);
+            message = _self.parseLinks(message);
             message = filter.clean(message);
             message = emoji.parse(message, "/emoji");
             users.forEach((data) => data.socket.emit("message", {
@@ -45,6 +46,31 @@ class ChatManager {
     clean(str) {
         return str.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/'/g, "&#039;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     }
+
+    parseLinks(str) {
+        function norm(n) {
+            return (n + (str.length + 1)) % (str.length + 1);
+        }
+        var index = Math.min(norm(str.indexOf("http://")), norm(str.indexOf("https://")));
+        while (index != str.length) {
+            var end = str.indexOf(" ", index);
+            if (end == -1) end = str.length;
+
+            var urlText = str.substring(index, end);
+
+            var ending = str.substring(end);
+            str = str.substring(0, index) + "<a target=\"_blank\" rel=\"noopener noreferrer\"" +
+                "href=\"" + urlText + "\">" + urlText + "</a>";
+            var nextStart = str.length;
+
+            str += ending;
+
+            index = Math.min(norm(str.indexOf("http://", nextStart)),
+                norm(str.indexOf("https://", nextStart)));
+        }
+        return str;
+    }
+
 
 }
 
