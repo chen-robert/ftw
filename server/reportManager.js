aa632921871beb13e5a9c61f3fdd5e4cd453cc3f
gamester543 <gamesterrex@gmail.com>
2018-04-14 00:25:41 -0700

    Prevented dupe reports

--- server/reportManager.js
+++ server/reportManager.js
@@ -10,15 +10,31 @@ const reportSchem = new mongoose.Schema({
 module.exports.Report = mongoose.model("Report", reportSchem);
 
 module.exports.addReport = function (name, comment, ip, callback) {
-    module.exports.Report.create({
-        username: name,
-        comment: comment,
+    module.exports.Report.findOne({
+        username: {
+            $regex: new RegExp(name, "i")
+        },
+        comment: {
+            $regex: new RegExp(comment, "i")
+        },
         ip: ip
-    }, (err) => {
-        if (err) {
-            console.error("Problem with saving report");
-            console.error(err);
+    }).exec(function (err, data) {
+        if (err) throw err;
+        if (data) {
+            //Don't let the user know we've silently eaten their report
+            callback(err);
+        } else {
+            module.exports.Report.create({
+                username: name,
+                comment: comment,
+                ip: ip
+            }, (err) => {
+                if (err) {
+                    console.error("Problem with saving report");
+                    console.error(err);
+                }
+                callback(err);
+            });
         }
-        callback(err);
     });
 }
