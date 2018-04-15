239811f62954b8c500f44f22f27c13c47c39c8ad
Doinkboink <alexwei@outlook.com>
2018-04-14 18:03:10 -0700

    Added better problems
    xXx_RobertHub_xXx

--- server/game/problemUtils.js
+++ server/game/problemUtils.js
@@ -1,11 +1,46 @@
 const utils = {};
 
 utils.getProblem = function () {
+    let g = Math.floor(10 * Math.random());
+    
+    if (g >= 0 && g < 3){
+        let a = Math.floor(100 * Math.random());
+        let b = Math.floor(100 * Math.random());
+        return {
+            text: "wot is " + a + " + " + b + "?",
+            answer: "" + (a + b)
+        };
+    }
+    if (g >= 3 && g < 6){
+        let a = Math.floor(200 * Math.random());
+        let b = Math.floor(150 * Math.random());
+        return {
+            text: "wot is " + a + " - " + b + "?",
+            answer: "" + (a - b)
+        };
+    }
+    if (g >= 6 && g < 8){
+        let a = Math.floor(100 * Math.random());
+        let b = Math.floor(50 * Math.random());
+        return {
+            text: "wot is " + a + " * " + b + "?",
+            answer: "" + (a * b)
+        };
+    }
+    if (g >= 8 && g < 10){
+        let a = Math.floor(500 * Math.random());
+        let b = Math.floor(20 * Math.random());
+        return {
+            text: "wot is " + a + " mod " + b + "?",
+            answer: "" + (a % b)
+        };
+    }
+    
+    //this part should never actually run?
     let a = Math.floor(100 * Math.random());
-    let b = Math.floor(100 * Math.random());
     return {
-        text: "wot is " + a + " + " + b + "?",
-        answer: "" + (a + b)
+        text: "wot is " + a + "?",
+        answer: "" + (a)
     };
 }
 
